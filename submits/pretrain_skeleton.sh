a9bbfc829e631d7ece67d2a881b640bb68392f03
alexandertheus <alexander.theudahl@gmail.com>
2025-09-08 15:46:39 +0200

    Add pretrain skeleton script

---
+++ submits/pretrain_skeleton.sh
@@ -0,0 +1,20 @@
+python pretrain.py \
+    --gpus 4 \
+    --save-dir ./save/cf-$(date +%b%d-%H-%M-%Y) \
+    --max-seq-len 1200 \
+    --batch-size 16 \
+    --nlayers 6 \
+    --nheads 8 \
+    --embsize 256 \
+    --d-hi 512 \
+    --epochs 15 \
+    --lr 0.0001 \
+    --warmup-ratio-or-step 10000 \
+    --val-check-interval 0.5 \
+    --trunc-by-sample \
+    --loss mse \
+    --balance-primary tissue \
+    --balance-secondary technology \
+    --train-path pretraining_data/cancer_gpt \
+    --zero-percentages 0.2 0.4 0.6 \
+    --strategy='ddp_find_unused_parameters_true'
